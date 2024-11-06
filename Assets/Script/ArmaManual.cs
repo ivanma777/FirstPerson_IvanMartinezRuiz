d3d06ab52edbf5a1b1d9a85f8ef3c197b02b72a6
ivanma777 <ivamaruiz5604@gmail.com>
2024-11-06 17:15:38 +0100

    particulasBalas

---
+++ Assets/Script/ArmaManual.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmaManual : MonoBehaviour
+{
+    [SerializeField] private ArmaSO misDatos;
+    [SerializeField] private ParticleSystem system;
+    
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            system.Play();
+            if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, misDatos.distanciaAtaque ))
+            {
+                
+                Debug.Log(hitInfo.transform.name);
+            }
+
+        }
+    }
+}
---
+++ Assets/Script/ArmaSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Arma")]
+public class ArmaSO : ScriptableObject
+{
+    //DATOS
+
+    public int balasCargador;
+    public int balasBolsa;
+    public float cadenciaAtaque;
+    public float distanciaAtaque;
+    public float danhoAtaque;
+
+
+
+}
